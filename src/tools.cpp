13449f82994d0607a4c9a2f43d550b7be709ad81
Andrey Yaroshenko <kradio3@gmail.com>
2018-11-19 15:56:54 +0300

    feat: implement laser only

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,10 +11,24 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  int e_size = estimations.size();
+
+  VectorXd rmse = VectorXd(4);
+  rmse << 0, 0, 0, 0;
+  if (e_size < 1 || e_size != ground_truth.size()) return rmse;
+
+  VectorXd summ(4);
+  summ << 0, 0, 0, 0;
+  for (int i=0; i<e_size; i++) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  rmse = rmse / e_size;
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +36,5 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+  return MatrixXd(7,7);
 }
